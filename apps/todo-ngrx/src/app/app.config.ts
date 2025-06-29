adfaaf3f9ea4b35c456cc9457e80a6eae988f683
EngiBeerHub <engibeer0422@gmail.com>
2025-06-30 06:07:50 +0900

    FIX: Temporarily comment out Redux dev tools configs

--- apps/todo-ngrx/src/app/app.config.ts
+++ apps/todo-ngrx/src/app/app.config.ts
@@ -16,13 +16,14 @@ import { provideStoreDevtools } from '@ngrx/store-devtools';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideStore(),
-    provideStoreDevtools({
-      maxAge: 25, // Retains last 25 states
-      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
-      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
-      connectInZone: true, // If set to true, the connection is established within the Angular zone
-    }),
+    provideStoreDevtools(),
+    //   {
+    //   maxAge: 25, // Retains last 25 states
+    //   autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    //   trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+    //   traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    //   connectInZone: true, // If set to true, the connection is established within the Angular zone
+    // }
     provideState({ name: todoFeatureKey, reducer: todoReducer }),
     provideEffects(TodoEffects),
     // provideAnimations(),
